dd4395df1aae913a06a98fbe0544fa66ff1c5bfe
hederithamar <heder.ithamar.hr@gmail.com>
2016-07-30 09:38:33 +0000

    cambiado el index

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,6 +10,7 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+
 Route::resource('admin','FrontController');
 Route::resource('/','IndexController');
 Route::resource('log','LogController');
